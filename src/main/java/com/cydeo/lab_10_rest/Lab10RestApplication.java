4fd4f22ab58bcc5dc290f331c4494ca1edf16f23
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-25 19:38:18 +0400

    Initial commit

---
+++ src/main/java/com/cydeo/lab_10_rest/Lab10RestApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.lab_10_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab10RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab10RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cydeo/lab_10_rest/Lab10RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.lab_10_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
