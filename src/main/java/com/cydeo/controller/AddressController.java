c27a7ab4bc41a7e0b766f34edb4fae4b10182747
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-26 17:52:43 +0400

    AddressController

--- src/main/java/com/cydeo/controller/AddressController.java
+++ src/main/java/com/cydeo/controller/AddressController.java
@@ -1,6 +1,24 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.AddressDTO;
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.service.AddressService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/address")
 public class AddressController {
+
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
 
 
     /*
@@ -14,6 +32,18 @@ public class AddressController {
      "data":<address data>
     */
 
+    @GetMapping("/{addressNo}")
+    public ResponseEntity<ResponseWrapper> getAddress(@PathVariable String addressNo){
+        AddressDTO foundAddress = addressService.findByAddressNo(addressNo);
+        ResponseWrapper responseWrapper = ResponseWrapper.builder()
+                .success(true)
+                .message("Address <addressNo> is successfully retrieved.")
+                .code(HttpStatus.OK.value())
+                .data(foundAddress)
+                .build();
+
+        return ResponseEntity.ok().body(responseWrapper);
+    }
 
 
     /*
