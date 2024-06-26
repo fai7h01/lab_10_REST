21d152e23377aa8c5feb861b84acf0ebdc47b5be
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-26 19:07:11 +0400

    StudentController

--- src/main/java/com/cydeo/controller/StudentController.java
+++ src/main/java/com/cydeo/controller/StudentController.java
@@ -1,7 +1,22 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.StudentDTO;
+import com.cydeo.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/student")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
      /*
            Endpoint: /api/v1/student
@@ -14,6 +29,15 @@ public class StudentController {
            "data":<students data>
      */
 
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getAllStudents(){
+        return ResponseEntity.ok(ResponseWrapper.builder()
+                .success(true)
+                .message("Students are successfully retrieved.")
+                .code(HttpStatus.OK.value())
+                .data(studentService.findAll()).build());
+
+    }
 
     /*
           Endpoint: /api/v1/student
@@ -25,6 +49,16 @@ public class StudentController {
           "code":201
           "data":<created student data>
     */
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createStudent(@RequestBody StudentDTO studentDTO){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ResponseWrapper.builder()
+                        .success(true)
+                        .message("Student is successfully created.")
+                        .code(HttpStatus.CREATED.value())
+                        .data(studentService.createStudent(studentDTO)).build());
+    }
 
 
 }
