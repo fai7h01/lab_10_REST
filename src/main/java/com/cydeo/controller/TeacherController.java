1fe800404f25822854fd6c5c98c54f30059c21f9
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-26 19:23:04 +0400

    StudentController

--- src/main/java/com/cydeo/controller/TeacherController.java
+++ src/main/java/com/cydeo/controller/TeacherController.java
@@ -1,6 +1,23 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/teacher")
 public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
 
          /*
            Endpoint: /api/v1/teacher
@@ -8,6 +25,11 @@ public class TeacherController {
            JSON Response Body:
            <teachers data>
         */
+
+    @GetMapping
+    public List<TeacherDTO> getAllTeachers(){
+        return teacherService.findAll();
+    }
 
         /*
            Endpoint: /api/v1/teacher/{username}
@@ -20,6 +42,15 @@ public class TeacherController {
            "data":<teacher data>
         */
 
+    @GetMapping("/{username}")
+    public ResponseEntity<ResponseWrapper> getTeacher(@PathVariable("username") String username){
+        return ResponseEntity.ok(ResponseWrapper.builder()
+                .success(true)
+                .message("Teacher is successfully retrieved.")
+                .code(HttpStatus.OK.value())
+                .data(teacherService.findByUsername(username)).build());
+    }
+
        /*
            Endpoint: /api/v1/teacher
            HTTP Status Code: 201
@@ -31,5 +62,17 @@ public class TeacherController {
            "code":201
            "data":<created teacher data>
      */
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createTeacher(@RequestBody TeacherDTO teacherDTO){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("teacherUsername", teacherDTO.getUsername())
+                .body(ResponseWrapper.builder()
+                        .success(true)
+                        .message("")
+                        .code(HttpStatus.CREATED.value())
+                        .data(teacherService.createTeacher(teacherDTO)).build());
+    }
 
 }
--- src/main/java/com/cydeo/dto/TeacherDTO.java
+++ src/main/java/com/cydeo/dto/TeacherDTO.java
@@ -2,6 +2,9 @@ package com.cydeo.dto;
 
 import com.cydeo.enums.EducationLevel;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,8 +14,11 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TeacherDTO {
 
+    @JsonIgnore
     private Long id;
 
     private String firstName;
@@ -21,6 +27,7 @@ public class TeacherDTO {
     private String email;
     private String username;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     private LocalDate birthday;
