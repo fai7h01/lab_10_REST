320e4e13705fbd4062ace46f6f1b3badb9a0a066
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-26 18:00:40 +0400

    AddressController

--- src/main/java/com/cydeo/controller/AddressController.java
+++ src/main/java/com/cydeo/controller/AddressController.java
@@ -5,10 +5,7 @@ import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.AddressService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/address")
@@ -34,15 +31,11 @@ public class AddressController {
 
     @GetMapping("/{addressNo}")
     public ResponseEntity<ResponseWrapper> getAddress(@PathVariable String addressNo){
-        AddressDTO foundAddress = addressService.findByAddressNo(addressNo);
-        ResponseWrapper responseWrapper = ResponseWrapper.builder()
+        return ResponseEntity.ok(ResponseWrapper.builder()
                 .success(true)
                 .message("Address <addressNo> is successfully retrieved.")
                 .code(HttpStatus.OK.value())
-                .data(foundAddress)
-                .build();
-
-        return ResponseEntity.ok().body(responseWrapper);
+                .data(addressService.findByAddressNo(addressNo)).build());
     }
 
 
@@ -52,6 +45,11 @@ public class AddressController {
       JSON Response Body:
       <updated address data>
      */
+
+    @PutMapping("/{addressNo}")
+    public AddressDTO updateAddress(@PathVariable String addressNo, @RequestBody AddressDTO addressDTO){
+        return addressService.update(addressNo,addressDTO);
+    }
 
 
 }
