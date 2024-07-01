5f6ed1f7ae7ebaed356204735c21dbd7ba345ad8
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-01 15:26:17 +0400

    Consuming WeatherStack API

--- src/main/java/com/cydeo/client/WeatherClient.java
+++ src/main/java/com/cydeo/client/WeatherClient.java
@@ -1,6 +1,8 @@
 package com.cydeo.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 /*
 http://api.weatherstack.com/  --> baseUrl
 current? --> endpoint
@@ -12,5 +14,8 @@ query=Tbilisi --> query param 2
 public interface WeatherClient {
 
 
+    @GetMapping("/current")
+    Object getCurrentWeather(@RequestParam("access_key") String accessKey,
+                             @RequestParam("query") String city);
 
 }
